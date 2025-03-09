0884c76fa91d493f89fe2561e28edff57f1896a0
gjaku1031 <gjaku1031@gmail.com>
2025-03-09 23:08:27 +0900

    commit

--- src/main/java/com/beyond/backend/domain/user/service/AuthService.java
+++ src/main/java/com/beyond/backend/domain/user/service/AuthService.java
@@ -1,12 +1,6 @@
 package com.beyond.backend.domain.user.service;
 
 import com.beyond.backend.domain.user.dto.*;
-import com.beyond.backend.domain.user.entity.User;
-import com.beyond.backend.domain.user.repository.UserRepository;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 
 
 public interface AuthService {
