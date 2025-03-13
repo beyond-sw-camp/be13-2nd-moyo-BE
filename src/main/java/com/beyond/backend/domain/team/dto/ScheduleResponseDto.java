3b13826be5a70f186d1a6184da38b2bdefa07cef
dh0522 <gong4857@naver.com>
2025-03-13 17:15:43 +0900

    [홍도현] Schedule 로직 수정

--- src/main/java/com/beyond/backend/domain/team/dto/ScheduleResponseDto.java
+++ src/main/java/com/beyond/backend/domain/team/dto/ScheduleResponseDto.java
@@ -3,10 +3,14 @@ package com.beyond.backend.domain.team.dto;
 import com.beyond.backend.domain.team.entity.Schedule;
 import com.beyond.backend.domain.team.entity.ScheduleStatus;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
 public class ScheduleResponseDto {
 
     private String title;
