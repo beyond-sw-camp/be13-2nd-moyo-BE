a0a71406fc892073f1dfa278ca8723253250a54a
gjaku1031 <gjaku1031@gmail.com>
2025-03-13 10:52:43 +0900

    Changes by dohyunhong

--- src/main/java/com/beyond/backend/domain/team/repository/ScheduleRepository.java
+++ src/main/java/com/beyond/backend/domain/team/repository/ScheduleRepository.java
@@ -1,10 +1,7 @@
-package com.beyond.backend.domain.project.repository;
+package com.beyond.backend.domain.team.repository;
 
 import com.beyond.backend.domain.project.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long>, ScheduleRepositoryCustom{
 }
