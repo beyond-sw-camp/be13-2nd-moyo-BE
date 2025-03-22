3a8ab1c2f527a236f4602da9dcbf3f22a38648e6
limhyunjo <sibagyeoni@gmail.com>
2025-03-22 21:25:51 +0900

    [임현조] 댓글 컨트롤러 수정

--- src/main/java/com/beyond/backend/controller/CommentController.java
+++ src/main/java/com/beyond/backend/controller/CommentController.java
@@ -145,14 +145,14 @@ public class CommentController {
 
     // 댓글 좋아요 / 좋아요 취소
     @Operation(summary = "댓글 좋아요 추가 및 취소", description = "댓글 좋아요 상태 (추가, 취소)")
-    @PostMapping("/{commentNo}/like")
+    @PostMapping("/comments/{commentNo}/like")
     public ResponseEntity<String> likeComment(@PathVariable Long commentNo) {
         String result = likeService.toggleCommentLike(commentNo);
         return ResponseEntity.ok(result);
     }
 
     @Operation(summary = "좋아요 갯수 조회", description = "댓그 좋아요 갯수 조회")
-    @GetMapping("/{commentNo}/like-count")
+    @GetMapping("/comments/{commentNo}/like-count")
     public ResponseEntity<Long> getLike(@PathVariable Long commentNo) {
         Long likeCount = likeService.getLikeCount(commentNo);
         return ResponseEntity.ok(likeCount);
