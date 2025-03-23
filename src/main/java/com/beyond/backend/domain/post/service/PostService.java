c30f27fb54e72ea3c05e86d95109c05e5d1925ad
limhyunjo <sibagyeoni@gmail.com>
2025-03-24 05:01:28 +0900

    [임현조] post, comment, project, tech 코드 리팩터링

--- src/main/java/com/beyond/backend/domain/post/service/PostService.java
+++ src/main/java/com/beyond/backend/domain/post/service/PostService.java
@@ -6,10 +6,10 @@ import com.beyond.backend.domain.post.dto.UserPostResponseDto;
 import com.beyond.backend.domain.post.entity.BoardType;
 import com.beyond.backend.domain.post.entity.PostSearchOption;
 import com.beyond.backend.domain.post.entity.PostSortOption;
-import com.beyond.backend.domain.post.entity.PostStatus;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 /**
  * <p>
@@ -27,7 +27,9 @@ import org.springframework.data.domain.Pageable;
  * 25. 2. 2.        hyunjo             최초 생성
  * 25. 2. 17.       hyunjo             내용 수정
  * 25. 2. 20.       hyunjo             내용 수정
- */public interface PostService {
+ */
+@Service
+public interface PostService {
 
 
     // 게시글 검색 + 전체 조회
@@ -37,20 +39,21 @@ import org.springframework.data.domain.Pageable;
     UserPostResponseDto getPostById(Long postNo);
 
     // 게시글 생성
-    PostResponseDto createPost(BoardType boardType, PostDto postDto);
+    PostResponseDto createPost(BoardType boardType, PostDto postDto, Long userNo);
 
     // 게시글 수정
     PostResponseDto updatePost(Long postNo, PostDto postDto);
 
     // 게시글 삭제
-    void deletePost(Long postN, Long userNo) ;
+    void deletePost(Long postNo) ;
 
     // 내가 쓴 게시글 전체 조회
-    Page<UserPostResponseDto> getUserPosts(BoardType boardType, Pageable pageable);
+    Page<UserPostResponseDto> getUserPosts(BoardType boardType, Pageable pageable, Long userNo);
 
     // 공지 게시판인 경우 관리자인지 검증
     void validatePostAuthority(BoardType boardType);
 
+    // 조회수
     void viewPost(Long postNo, HttpServletRequest request);
 
 }
--- src/main/java/com/beyond/backend/domain/project/repository/ProjectRepositoryImpl.java
+++ src/main/java/com/beyond/backend/domain/project/repository/ProjectRepositoryImpl.java
@@ -6,11 +6,16 @@ import static com.beyond.backend.domain.project.entity.QProjectTech.*;
 import static com.beyond.backend.domain.team.entity.QTeam.*;
 import static com.beyond.backend.domain.teamUser.entity.QTeamUser.*;
 
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.beyond.backend.domain.project.entity.QProject;
+import com.beyond.backend.domain.team.entity.QTeam;
+import com.beyond.backend.domain.team.entity.TeamJoinStatus;
+import com.beyond.backend.domain.teamUser.entity.QTeamUser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
@@ -149,6 +154,8 @@ public class ProjectRepositoryImpl implements ProjectRepositoryCustom {
 				Collectors.mapping(tuple -> tuple.get(projectTech.tech.techName), Collectors.toList()) // techName 리스트 매핑
 			));
 	}
+
+	// 프로젝트
 
 
 	private BooleanExpression searchOption(String keyword, ProjectSearchOption option){
