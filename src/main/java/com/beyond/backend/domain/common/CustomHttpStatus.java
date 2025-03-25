2b015a92dcbc85f74c602a7052385b92a9410dfc
dh0522 <gong4857@naver.com>
2025-03-25 16:06:27 +0900

    [최건] 커스텀 에러 메시지

---
+++ src/main/java/com/beyond/backend/domain/common/CustomHttpStatus.java
@@ -0,0 +1,8 @@
+package com.beyond.backend.domain.common;
+
+import org.springframework.http.HttpStatus;
+
+public class CustomHttpStatus {
+
+	public static final HttpStatus BANNED = HttpStatus.valueOf(499);
+}
