1a84b1437e37c66185fefffaa4ab1983c12dad80
dh0522 <gong4857@naver.com>
2025-03-25 16:42:06 +0900

    [최건] 에러 메시지 수정

--- src/main/java/com/beyond/backend/domain/common/exception/message/ExceptionMessage.java
+++ src/main/java/com/beyond/backend/domain/common/exception/message/ExceptionMessage.java
@@ -32,8 +32,8 @@ public enum ExceptionMessage {
     USER_NOT_FOUND("USER_001", "해당 유저를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     USER_ACCESS_DENIED("USER_002", "해당 유저는 접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
     USER_INPUT_MISMATCH("USER_003", "아이디 또는 비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
-    USER_LOCKED("USER_004", "계정이 잠겼습니다. 관리자에게 문의하세요.", HttpStatus.FORBIDDEN),
-    USER_BANED("USER_005", "계정이 정지되었습니다. 관리자에게 문의하세요.", CustomHttpStatus.BANNED),
+    USER_LOCKED("USER_004", "계정이 잠겼습니다. 관리자에게 문의하세요.", HttpStatus.LOCKED),
+    USER_BANED("USER_005", "계정이 정지되었습니다. 관리자에게 문의하세요.", HttpStatus.FORBIDDEN),
     USER_DUPLICATE("USER_006", "이미 존재하는 유저입니다.", HttpStatus.BAD_REQUEST),
 
     // 신고 관련
