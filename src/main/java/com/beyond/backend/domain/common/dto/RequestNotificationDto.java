b82e153e242c654dbcaa9cb644243aa09023f95d
gjaku1031 <gjaku1031@gmail.com>
2025-03-25 12:19:23 +0900

    [최건] 알림 uuid 수정

--- src/main/java/com/beyond/backend/domain/common/dto/RequestNotificationDto.java
+++ src/main/java/com/beyond/backend/domain/common/dto/RequestNotificationDto.java
@@ -9,12 +9,14 @@ import lombok.Data;
 @AllArgsConstructor
 public class RequestNotificationDto {
 
+    private String id;
     private String senderId;    // 보낸 사람 ID
     private String receiverId;  // 받는 사람 ID
     private NotificationType notificationType;
     private String message;     // 알림 메시지
 
     public RequestNotificationDto(Notification notification) {
+        this.id = notification.getId();
         this.senderId = notification.getSenderId();
         this.receiverId = notification.getReceiverId();
         this.notificationType = notification.getType();
