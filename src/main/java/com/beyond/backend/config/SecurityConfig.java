dd22492e014400ec40198658c5c67e08f9259c51
gjaku1031 <gjaku1031@gmail.com>
2025-03-27 09:14:06 +0900

    [최건] admin login

--- src/main/java/com/beyond/backend/config/SecurityConfig.java
+++ src/main/java/com/beyond/backend/config/SecurityConfig.java
@@ -32,7 +32,6 @@ import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
-//@EnableMethodSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -92,9 +91,9 @@ public class SecurityConfig {
                                 "/posts/search",             // 게시글 검색
                                 "/project",                  // 프로젝트 전체 조회
                                 "/project/{projectNo}",      // 프로젝트 상세 조회
-                                "/project/search"    // 프로젝트 검색
+                                "/project/search",    // 프로젝트 검색
+                                "/admin/login"
                         ).permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 );
         return http.build();
